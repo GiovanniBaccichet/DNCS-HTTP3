6e303ab1ffdd737f43c8d20738a0adf222e1c7cf
Giovanni Baccichet <32038133+GiovanniBaccichet@users.noreply.github.com>
2021-01-16 22:56:32 +0100

    Added a script to start streaming
    This script runs ffmpeg on all video-streaming containers (that are running)

--- docker/docker_deploy.sh
+++ docker/docker_deploy.sh
@@ -13,7 +13,7 @@ echo "|                                                        BUILDING IMAGES  
 echo "+----------------------------------------------------------------------------------------------------------------------------------+"
 
 echo "=> WEB PAGE"
-#docker build --no-cache -t quiche-text -f Dockerfile_TEXT .
+docker build --no-cache -t quiche-text -f Dockerfile_TEXT .
 
 echo "=> VIDEO STREAMING"
 docker build --no-cache -t quiche-video -f Dockerfile_VIDEO .
@@ -64,19 +64,19 @@ echo "|                                                       RUNNING CONTAINERS
 echo "+----------------------------------------------------------------------------------------------------------------------------------+"
 
 echo "[TCP]: Web page"
-#docker run --name tcp-text -d -p $h1TEXTp1:80 -p $h1TEXTp2:443/tcp -p $h1TEXTp2:443/udp -v $PWD/confs/tcp.text.nginx.conf:/etc/nginx/nginx.conf quiche-text
+docker run --name tcp-text -d -p $h1TEXTp1:80 -p $h1TEXTp2:443/tcp -p $h1TEXTp2:443/udp -v $PWD/confs/tcp.text.nginx.conf:/etc/nginx/nginx.conf quiche-text
 
 echo "[TCP]: Video streaming"
 docker run --name tcp-video -d -p $h1VIDEOp1:80 -p $h1VIDEOp2:443/tcp -p $h1VIDEOp2:443/udp -v $PWD/confs/tcp.video.nginx.conf:/etc/nginx/nginx.conf quiche-video
 
 echo "[HTTP/2]: Web page"
-#docker run --name http2-text -d -p $h2TEXTp1:80 -p $h2TEXTp2:443/tcp -p $h2TEXTp2:443/udp -v $PWD/confs/http2.text.nginx.conf:/etc/nginx/nginx.conf quiche-text
+docker run --name http2-text -d -p $h2TEXTp1:80 -p $h2TEXTp2:443/tcp -p $h2TEXTp2:443/udp -v $PWD/confs/http2.text.nginx.conf:/etc/nginx/nginx.conf quiche-text
 
 echo "[HTTP/2]: Video streaming"
 docker run --name http2-video -d -p $h2VIDEOp1:80 -p $h2VIDEOp2:443/tcp -p $h2VIDEOp2:443/udp -v $PWD/confs/http2.video.nginx.conf:/etc/nginx/nginx.conf quiche-video
 
 echo "[HTTP/3]: Web page"
-#docker run --name http3-text -d -p $h3TEXTp1:80 -p $h3TEXTp2:443/tcp -p $h3TEXTp2:443/udp -v $PWD/confs/http3.text.nginx.conf:/etc/nginx/nginx.conf quiche-text
+docker run --name http3-text -d -p $h3TEXTp1:80 -p $h3TEXTp2:443/tcp -p $h3TEXTp2:443/udp -v $PWD/confs/http3.text.nginx.conf:/etc/nginx/nginx.conf quiche-text
 
 echo "[HTTP/3]: Video streaming"
 docker run --name http3-video -d -p $h3VIDEOp1:80 -p $h3VIDEOp2:443/tcp -p $h3VIDEOp2:443/udp -v $PWD/confs/http3.video.nginx.conf:/etc/nginx/nginx.conf quiche-video
