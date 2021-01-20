6f4063d40818d85e568e09aa95f60fa80125c0b9
Giovanni Baccichet <32038133+GiovanniBaccichet@users.noreply.github.com>
2021-01-20 22:15:45 +0100

    Google lighthouse installation

--- vagrant/client.sh
+++ vagrant/client.sh
@@ -5,3 +5,10 @@ sudo ip addr add 192.168.1.2/30 dev enp0s8
 sudo ip link set dev enp0s8 up
 
 sudo ip route add 192.168.2.0/30 via 192.168.1.1
+
+# (Google) Lighthouse installation
+sudo apt-get update
+sudo apt-get -y install nodejs npm chromium-browser
+sudo npm install n -g
+sudo n stable
+sudo npm install -g lighthouse
