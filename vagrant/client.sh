a676cea8e3e6b6f91016c08883b1ef13faedd667
GiovanniBaccichet <baccichetgiovanni@icloud.com>
2021-02-11 12:16:48 +0100

    Don’t need Firefox anymore

--- vagrant/client.sh
+++ vagrant/client.sh
@@ -15,7 +15,6 @@ wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-ke
 sudo sh -c 'echo "deb https://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 sudo apt-get update
 sudo apt-get -y install google-chrome-stable
-sudo apt-get -y install firefox # to be removed
 
 # (Google) Lighthouse installation
 sudo apt-get -y install nodejs npm
